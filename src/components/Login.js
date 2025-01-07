a661cbd09a262575059abc8a6ff683b061008680
Giancarlol <gian_pc21@hotmail.com>
2025-01-07 20:04:40 +0100

    solving deploy probem css log in

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useFirebase } from '../contexts/FirebaseContext';
-import './Login.css';
+import './Auth.css';
 
 const Login = () => {
   const navigate = useNavigate();
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useFirebase } from '../contexts/FirebaseContext';
-import './Register.css';
+import './Auth.css';
 
 const Register = () => {
   const navigate = useNavigate();
