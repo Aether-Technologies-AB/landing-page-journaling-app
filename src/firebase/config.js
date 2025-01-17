3ca0be1eaa368f0375bb1e72b220201762bb957e
Giancarlol <gian_pc21@hotmail.com>
2025-01-17 21:05:38 +0100

    test: hardcode Firebase config for testing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -23,13 +23,14 @@ console.log('Environment:', {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
+// Hardcoded config for testing
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDAqieq7SssPjNBdgWIHS8l6cP0ipWhJ4Y",
+  authDomain: "whisperjournal-bbe7d.firebaseapp.com",
+  projectId: "whisperjournal-bbe7d",
+  storageBucket: "whisperjournal-bbe7d.appspot.com",
+  messagingSenderId: "245116053413",
+  appId: "1:245116053413:web:b110438ae2408dc54a2a4c"
 };
 
 console.log('Firebase Config:', firebaseConfig);
